72c9233dbffdfe29470a1e53996278c30911c53f
LaurenGeis <72772740+LaurenGeis@users.noreply.github.com>
2020-10-23 19:10:34 -0700

    Delete PrintStrings.java

--- W1/Demo1NumbersTest/PrintStrings.java
+++
@@ -1,9 +0,0 @@
-package Demo1NumbersTest;
-import static java.lang.System.out;
-public class PrintStrings {
-    
-    public static void main(String... args) {
-        var name = "John Smith";
-        out.println(name);
-    }
-}
