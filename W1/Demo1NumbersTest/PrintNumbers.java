b551f0128a531d3cb863ab75ada803bd2b0a045d
LaurenGeis <72772740+LaurenGeis@users.noreply.github.com>
2020-10-23 19:10:27 -0700

    Delete PrintNumbers.java

--- W1/Demo1NumbersTest/PrintNumbers.java
+++
@@ -1,9 +0,0 @@
-package Demo1NumbersTest;
-import static java.lang.System.out;
-public class PrintNumbers {
-    
-    public static void main(String... args) {
-        var number = 0;
-        out.println(number);
-    }
-}
